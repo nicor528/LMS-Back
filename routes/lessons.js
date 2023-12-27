8c151e8a46b5951a52be0276ff13f0873d85523c
nicor528 <nicolasriquelme528@gmail.com>
2023-12-27 19:17:21 -0300

    27-12-23

--- routes/lessons.js
+++ routes/lessons.js
@@ -9,8 +9,8 @@ router.get("/getCourseLessons", async (req,res) => {
         getCourseLessons(courseID).then(async (lessons) => {
             res.status(200).send({data: lessons, status: true})
         })*/
-        getCourses().then(data => {
-            const course = data.data.filter(item => item.attributes.course_ID === course_ID)
+        getCourses().then(async (data) => {
+            const course = await data.data.filter(item => item.attributes.course_ID === course_ID)
             console.log(course[0])
             res.status(200).send({data: course[0].attributes.lms_lessons.data, status: true})
         }).catch(error => {res.status(400).send({error, status: false})})
