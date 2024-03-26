43dcf8b3bee222b6e95beda9d5ed9b977c5c96db
nicor528 <nicolasriquelme528@gmail.com>
2024-03-26 11:25:44 -0300

    26-3-24

--- routes/SingIn.js
+++ routes/SingIn.js
@@ -74,6 +74,7 @@ router.get("/getUserInfo", (req, res) => {
         getUser2(user_ID).then(user => {
             getAllUserCourses().then(async (data) => {
                 console.log("test1")
+                console.log(user)
                 let user1 = await user;
                 const allCourses = await data.data.filter(data => data.attributes.user_ID === user_ID)
                 user1.data.attributes.lms_user_courses = allCourses.length > 0 ? allCourses : [];
