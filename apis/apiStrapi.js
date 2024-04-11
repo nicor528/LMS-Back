da92c19163418e9826e16093fe9cf24fd73de190
nicor528 <nicolasriquelme528@gmail.com>
2024-04-11 12:46:39 -0300

    4-11-24

--- apis/apiStrapi.js
+++ apis/apiStrapi.js
@@ -135,7 +135,7 @@ async function getUser3(id) {
 function getCourses () {
     return(
         new Promise (async (res, rej) => {
-            fetch(`${process.env.url}/lms-courses?populate[lms_modules][populate]=*`, {
+            fetch(`${process.env.url}/lms-courses?populate=*`, {
                 method: "GET",
                 headers: {
                     Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
