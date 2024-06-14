2e0403ea525b2ac16cb717a0dd09c9d4715a38ad
nicor528 <nicolasriquelme528@gmail.com>
2024-06-14 11:49:24 -0300

    146-24

--- apis/apiStrapi.js
+++ apis/apiStrapi.js
@@ -342,7 +342,7 @@ function vinculateCourse (userID, courseID, course, n_lessons) {
                         percentage: 0,
                         courseID: courseID,
                         name: course.attributes.technology,
-                        imageUrl: "https://cms.pragra.io" + course.attributes.imageUrl.data.attributes.url
+                        imageUrl: "https://cms.pragra.io" + course.attributes.imageUrl.data ?  course.attributes.imageUrl.data.attributes.url : ""
                     }
                 })
             }).then(async (result) => {
